cdb5b67594dee19121a30936f0c40aa649fcba55
Muhammad Omer Ali Malik <omeralimalik96@gmail.com>
2022-02-02 22:14:53 +0500

    Added a node js client to test all of the API endpoints

--- client/client.js
+++ client/client.js
@@ -3,7 +3,10 @@ const http = require('axios')
 const website = "http://localhost:8080/"
 
 
-
+/*
+Tests the /users/create POST METHOD
+Also Tests the /users/search/:userid GET METHOD
+*/
 http.post(website + 'users/create' , {
         email: "omeralimalik96@gmail.com",
         name: "Im secure",
@@ -21,6 +24,9 @@ http.post(website + 'users/create' , {
         console.log(err)
     })
 
+/**
+ * Tests the /users/authenticate method
+ */
 http.post(website + "users/authenticate" , {
     userId : "two",
     password: "3717733578",
@@ -28,6 +34,22 @@ http.post(website + "users/authenticate" , {
 }).then(resp => {
     console.log("authentication succesful!")
     console.log("Results of authentication + "  + resp.data);
+}).catch(error => {
+    if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+    }
+})
+
+/**
+ * Tests the /users/update/:userid method
+ */
+
+http.post(website + "users/update/two" , {
+    email: "My email is updated!"
+}).then(resp => {
+    console.log(resp.data);
 }).catch(error => {
     if (error.response) {
         console.log(error.response.data);
