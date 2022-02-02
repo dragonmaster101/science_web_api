804f942ddc46d111763894ce91cb968b64a2f2b0
Muhammad Omer Ali Malik <omeralimalik96@gmail.com>
2022-02-02 19:16:02 +0500

    Built API for the first time

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-
 	"fmt"
 
-	"net/http"
 	"encoding/json"
+	"net/http"
 
-	"github.com/dragonmaster101/acweb_api/database"	
+	"github.com/dragonmaster101/acweb_api/database"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,82 +18,80 @@ import (
 	END END END END OF PATH CONSTANTS
 */
 
-
-
 func main() {
-	router := gin.Default();
+	router := gin.Default()
 
-	db := database.Setup();
+	db := database.Setup()
 
-	router.GET("/users/search/:userid" , func(c *gin.Context) {
-		userID := c.Param("userid");
-
-		account , fetchErr := db.GetUserInfo(userID);
-		if fetchErr != nil {
-			errString := fmt.Sprintf("an error occured while fetching account from database , err : %v" , fetchErr);
-			c.String(http.StatusBadRequest , errString);
-			return;
-		}
-
-		jsonData , jsonConversionError := json.Marshal(account);
-		if jsonConversionError != nil {
-			c.String(http.StatusBadRequest , "internal error converting account data into json , err : %v" , jsonConversionError);
-			return;
-		}
-
-		c.Data(http.StatusOK , gin.MIMEJSON , jsonData);
-	})	
-
-	router.POST("/users/create" , func(c *gin.Context){
-		account := database.Account{};
-		
-		decodeErr := json.NewDecoder(c.Request.Body).Decode(&account);
-		if decodeErr != nil {
-			c.String(http.StatusBadRequest , 
-				"error decoding the request body make sure you followed the proper format , err : %v" , decodeErr);
-			return;
-		}
-
-		postErr := db.PostUserInfo(&account);
-		if postErr != nil {
-			c.String(http.StatusBadRequest , "error posting the account details to the database , errr : %v" , postErr)
-			return;
-		}	
-
-		c.String(http.StatusOK , "Successfully created the account")
-	});
-
-	router.POST("users/update/:userid" , func(c * gin.Context){
-		
+	router.GET("/users/search/:userid", func(c *gin.Context) {
 		userID := c.Param("userid")
-		account := &database.NilAccount{};
 
-		updateErr := db.UpdateUserInfo(userID , account);	
-		if updateErr != nil {
-			c.String(http.StatusBadRequest , "error updating the account make sure you followed the proper format , err : %v" , updateErr);
-			return;
+		account, fetchErr := db.GetUserInfo(userID)
+		if fetchErr != nil {
+			errString := fmt.Sprintf("an error occured while fetching account from database , err : %v", fetchErr)
+			c.String(http.StatusBadRequest, errString)
+			return
 		}
 
-		c.String(http.StatusOK , "Successfully updated the account with the given userID");
+		jsonData, jsonConversionError := json.Marshal(account)
+		if jsonConversionError != nil {
+			c.String(http.StatusBadRequest, "internal error converting account data into json , err : %v", jsonConversionError)
+			return
+		}
+
+		c.Data(http.StatusOK, gin.MIMEJSON, jsonData)
 	})
 
-	router.POST("users/authenticate" , func (c *gin.Context){
-		form := database.AuthenticatorForm{};
+	router.POST("/users/create", func(c *gin.Context) {
+		account := database.Account{}
 
-		decodeErr := json.NewDecoder(c.Request.Body).Decode(&form);
+		decodeErr := json.NewDecoder(c.Request.Body).Decode(&account)
 		if decodeErr != nil {
-			c.String(http.StatusBadRequest , 
-				"error decoding the request body make sure you followed the proper format , err : %v" , decodeErr);
-			return;
+			c.String(http.StatusBadRequest,
+				"error decoding the request body make sure you followed the proper format , err : %v", decodeErr)
+			return
 		}
 
-		result , authenticationErr := db.AuthenticateUserInfo(&form);
+		postErr := db.PostUserInfo(&account)
+		if postErr != nil {
+			c.String(http.StatusBadRequest, "error posting the account details to the database , errr : %v", postErr)
+			return
+		}
+
+		c.String(http.StatusOK, "Successfully created the account")
+	})
+
+	router.POST("users/update/:userid", func(c *gin.Context) {
+
+		userID := c.Param("userid")
+		account := &database.NilAccount{}
+
+		updateErr := db.UpdateUserInfo(userID, account)
+		if updateErr != nil {
+			c.String(http.StatusBadRequest, "error updating the account make sure you followed the proper format , err : %v", updateErr)
+			return
+		}
+
+		c.String(http.StatusOK, "Successfully updated the account with the given userID")
+	})
+
+	router.POST("users/authenticate", func(c *gin.Context) {
+		form := database.AuthenticatorForm{}
+
+		decodeErr := json.NewDecoder(c.Request.Body).Decode(&form)
+		if decodeErr != nil {
+			c.String(http.StatusBadRequest,
+				"error decoding the request body make sure you followed the proper format , err : %v", decodeErr)
+			return
+		}
+
+		result, authenticationErr := db.AuthenticateUserInfo(&form)
 		if authenticationErr != nil {
-			c.String(http.StatusBadRequest , "error getting authentication results from the database , err : %v" , authenticationErr);
+			c.String(http.StatusBadRequest, "error getting authentication results from the database , err : %v", authenticationErr)
 		}
 
-		c.String(http.StatusOK , "%v" , result);
-	});
+		c.String(http.StatusOK, "%v", result)
+	})
 
-	router.Run();
+	router.Run()
 }
