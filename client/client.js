2b860aaacbaee29043d766507330a5e18a52fee1
Muhammad Omer Ali Malik <omeralimalik96@gmail.com>
2022-02-02 22:06:19 +0500

    Going to sleep

--- client/client.js
+++ client/client.js
@@ -6,12 +6,12 @@ const website = "http://localhost:8080/"
 
 http.post(website + 'users/create' , {
         email: "omeralimalik96@gmail.com",
-        name: "Im insecure",
-        password: "Thisisnothashed",
-        username: "one",
+        name: "Im secure",
+        password: "3717733579",
+        username: "three",
     }).then(response => {
         console.log("successfully created account getting it now")
-        http.get(website + "users/search/one").then(resp => {
+        http.get(website + "users/search/two").then(resp => {
             console.log("get request successful!")
             console.log(resp.data);
         }).catch(err => {
@@ -21,4 +21,19 @@ http.post(website + 'users/create' , {
         console.log(err)
     })
 
-console.log("Something is happening")
+http.post(website + "users/authenticate" , {
+    userId : "two",
+    password: "3717733578",
+    hashed: true,
+}).then(resp => {
+    console.log("authentication succesful!")
+    console.log("Results of authentication + "  + resp.data);
+}).catch(error => {
+    if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+    }
+})
+
+console.log("Started Program!")
